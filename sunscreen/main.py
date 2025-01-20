416255e20d1df588388b9871ac220917a6a34260
Mark Drago <markdrago@gmail.com>
2025-01-20 17:39:45 -0500

    install and execute black for code formatting

--- sunscreen/main.py
+++ sunscreen/main.py
@@ -2,16 +2,18 @@ import argparse
 
 import sunscreen.renderer
 
+
 def main():
-  print('From Main()')
-  args = parse_args()
-  renderer = sunscreen.renderer.Renderer(args.fullscreen)
-  renderer.run()
-  print('Quitting')
+    print("From Main()")
+    args = parse_args()
+    renderer = sunscreen.renderer.Renderer(args.fullscreen)
+    renderer.run()
+    print("Quitting")
+
 
 def parse_args():
-  parser = argparse.ArgumentParser(
-    prog='sunscreen',
-    description='Displays stats from an enphase solar system.')
-  parser.add_argument('-f', '--fullscreen', action='store_true')
-  return parser.parse_args()
+    parser = argparse.ArgumentParser(
+        prog="sunscreen", description="Displays stats from an enphase solar system."
+    )
+    parser.add_argument("-f", "--fullscreen", action="store_true")
+    return parser.parse_args()
